9cf8ab57d6d385df9a5dc741cf1eadf1767d8742
Alexander Richards <electrodeyt@gmail.com>
2020-04-24 00:27:01 +0200

    Finished initial instruction machine-file code

--- uasm/machine.cpp
+++ uasm/machine.cpp
@@ -19,13 +19,19 @@ MachineFile Machine::readMachine(std::string path) {
 	while (std::getline(ifstream, s)) {
 		std::vector<std::string> line = convertFileLineToVector(s);
 		if (line.empty()) { continue; }
-		for (int i = 0; i < line.size(); i++) {
-			std::cout << line.at(i);
-			if (i + 1 < line.size()) {
-				std::cout << "--";
+		if (line.at(0) == "inst") {
+			// instruction definition
+			Instruction new_inst;
+			new_inst.decodeInstLine(line);
+
+			std::cout << "Instruction name: " << new_inst.mnemonic << "\nInstruction bits: ";
+			for (int i = 0; i < new_inst.instruction.size(); i++) {
+				std::cout << new_inst.instruction.at(i);
 			}
+			std::cout << "\n";
+			// add the instruction to the machine
+			machine.instructions.push_back(new_inst);
 		}
-		std::cout << "\n";
 	}
 
 	return machine;
@@ -37,8 +43,49 @@ void Machine::Instruction::decodeInstLine(std::vector<std::string> line) {
 		std::cerr << "[Instruction] [BUG] Instruction line argument name is not 'inst'!\n";
 		return;
 	}
-	std::vector<std::string> instruction = Helper::splitStringByColon(line.at(1));
-
+	line.erase(line.begin()); // erase the "inst" entry
+	// decode the arguments aka. the part of the machine file that looks like the mnemonic
+	std::vector<std::string> instruction_arguments = Helper::splitStringByColon(line.at(0));
+	std::vector<char> argument_char; // argument names, used for converting the file bits to the class bits
+	mnemonic = instruction_arguments.at(0); // first entry is the mnenmonic
+	for (int i = 1; i < instruction_arguments.size(); i++) {
+		// get the argument bit count
+		std::vector<std::string> arg = Helper::splitStringByColon(line.at(i)); // conveniently because i is offset by 1 for the mnemonic we can just use it lol
+		if (arg.empty() || arg.size() == 1) {
+			std::cerr << "[Instruction] Did not find valid instruction argument bit count!\n";
+			exit(-1);
+		}
+		// convert the argument bit count
+		int bit_count = 0;
+		if (arg.at(1) == "r") { bit_count = 0xffff; } // 0xffff is the "bit count" for a register-only value
+		else { bit_count = std::stoi(arg.at(1)); }
+		arguments.push_back(bit_count);
+		// save the argument "name"
+		argument_char.push_back(arg.at(0).at(0));
+	}
+	// get the bits, should be the second to last
+	std::string bits = line.at(line.size() - 2);
+	instruction.clear();
+	for (int i = 0; i < bits.size(); i++) {
+		if (bits.at(i) == '0') {
+			// a zero
+			instruction.push_back(0);
+		} else if (bits.at(i) == '1') {
+			// a one
+			instruction.push_back(1);
+		} else {
+			// find the argument "id"
+			for (int x = 0; x < argument_char.size(); x++) {
+				//std::cout << "[DEBUG] checking argument_char " << x << ", " << argument_char.at(x) << "\n";
+				if (bits.at(i) == argument_char.at(x)) {
+					instruction.push_back(x + 2);
+					x = argument_char.size();
+				}
+			}
+		}
+	}
+	// get instruction type
+	instruction_type = std::stoi(line.at(line.size() - 1));
 }
 #pragma endregion
 
@@ -83,6 +130,5 @@ std::vector<std::string> Machine::convertFileLineToVector(std::string s) {
 
 	return ret;
 }
-std::vector<std::string> 
 #pragma endregion
 
--- uasm/machine.h
+++ uasm/machine.h
@@ -6,13 +6,16 @@ namespace Machine {
 	class Instruction {
 	public:
 		std::string mnemonic = "";
-		std::vector<int> arguments;
+		std::vector<int> arguments;		// bit count of argument, except when:
+										// 0xffff: register
 		std::vector<int> instruction;	// every entry is one bit
 										// 0: always 0
 										// 1: always 1
 										// 2+: arguments 0+
+		int instruction_type = 0;
 		/// Decode a file line into the instruction
-		void decodeInstLine(std::vector<std::string> line); 
+		void decodeInstLine(std::vector<std::string> line);
+
 	};
 	class MachineFile {
 	public:
